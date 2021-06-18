86a83aa6febd8fdf749c07437e38b5dcfd6543f7
FrancoCorporation <rodolfofrancoxico@gmail.com>
2021-06-17 23:32:57 -0300

    Removendo a chave secreta

--- Settings.cs
+++
@@ -1,8 +0,0 @@
-namespace condominioApi
-{
-    public static class Settings
-    {
-        //Chave para gerar token
-        public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
-    }
-}
